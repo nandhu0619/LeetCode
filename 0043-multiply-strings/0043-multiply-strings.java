61fed7ce2c2abfcbd2bfeb33c924d3fe5751c0a3
Nandhakumar U <117754469+nandhu0619@users.noreply.github.com>
2024-06-23 10:47:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int n=Integer.parseInt(num1);
+        int m=Integer.parseInt(num2);
+        int ans=m*n;
+        String s=Integer.toString(ans);
+        return s;
+    }
+
+}
